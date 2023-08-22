61a013ad403b217e6d3a9126723ca1b582fb365c
hyejin beck <beckhye@gmail.com>
2023-08-22 11:48:15 +0900

    특정인덱스 해당게시물 불러오기

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+# Post 모델(클래스/기능이있음) 을 import 불러온다. 
+from .models import Post
 
 # Create your views here.
 def index(request): 
-    return render(request,'index.html')
+    posts = Post.objects.all()
+    
+    context = {
+        'posts' : posts, 
+    }
+
+    return render(request,'index.html',context)
+
+def detail(request, id):
+    post = Post.objects.get(id=id)
+    # id가 id인 애를 가져와! 
+
+    context = {
+        'post': post, 
+    }
+
+    return render(request, 'detail.html', context)
