534798468b3c76c513348b0c621318902ba355db
hyejin beck <beckhye@gmail.com>
2023-08-22 13:47:21 +0900

    delate 지워야하는값

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 # Post 모델(클래스/기능이있음) 을 import 불러온다. 
 from .models import Post
 
@@ -21,3 +21,25 @@ def detail(request, id):
     }
 
     return render(request, 'detail.html', context)
+
+
+
+def new(request): 
+    return render(request, 'new.html')
+
+def create(request): 
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+
+    post = Post()
+    post.title = title 
+    post.content = content 
+    post.save()
+
+    return redirect(f'/posts/{post.id}')
+
+def delate(request.id): 
+    post= Post.objects.get(id=id)
+    post.delate()
+
+    return redirect('/index')
